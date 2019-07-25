ae319d3c9cf9e89a738dfc364318df413549834a
Rene Ismael <Ismael_br7@hotmail.com>
2019-07-24 23:58:19 -0500

    Obteniendo los datos de la API web

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./Header";
+
+//This data is only develop
+// API key
+// HMW7SSGNV2ZZV23CLP
+// Client secret
+// 4LUZ4RGZFUJI6ZJIJOYONEEM3GON7AOU5TFWSAYEH5YHYR6JE5
+// Private token
+// JA63LBZQ5SDYW3EZ2BFY
+// Public token
+// VGBSV5WC2BEPSJIPHVY3
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
